9211ac75e1cb42d6a716d35bcbb82a804128855e
Nishant-Xento <47963177+Nishant-Xento@users.noreply.github.com>
2019-07-24 12:11:01 +0530

    ___

--- src/FormRow.js
+++ src/FormRow.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 const style = {
   marginBottom: '24px'
 }
 
 const FormRow = ({ children }) => <div style={style}>{children}</div>
-
-FormRow.propTypes = {
-  children: PropTypes.any
-}
 
 export default FormRow
